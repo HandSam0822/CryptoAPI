e45f439ee93986f1373b52433ec64b4c4af64978
Liaoshousan <shousanl@andrew.cmu.edu>
2022-03-24 17:47:00 -0400

    add end to end unit test with cypress

---
+++ client/cypress/integration/sample_spec.js
@@ -0,0 +1,35 @@
+describe('Test cryptoAPI', () => {
+    beforeEach(()=> {
+        cy.visit('http://localhost:3000')
+    });
+    
+    it("check ten exchanges are on home page.", ()=> {
+        cy.get('.col').
+        should("have.length", 10)
+        cy.get('.route-btn').
+        should("have.length", 10)
+        cy.get('.route-btn').first().should('have.text', 'See Binance')
+        cy.get('.route-btn').last().should('have.text', 'See Huobi Global')
+    });
+
+    it("go to each profile page, check go back button works", () => {
+        for (var i = 0; i < 10; i++) {
+            cy.get(".route-btn").eq(i).click()
+            cy.url().should('include', '/profile')            
+            cy.get(".route-btn").should("have.text", "back to main page").click()                        
+            cy.url().should('include', '/')
+        }              
+    })
+    it("go to each profile page, check all info is inculded", () => {
+        for (var i = 0; i < 10; i++) {
+            cy.get(".route-btn").eq(i).click()
+            cy.contains("Trust score")
+            cy.contains("Year of establishment")
+            cy.contains("Social Media link")
+            cy.contains("Description")            
+            cy.get(".route-btn").eq(0).click()
+            cy.url().should('include', '/')
+        }    
+    })
+})
+  
---
+++ client/src/components/ProfileCard.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Card, Button } from 'react-bootstrap';
+import { useNavigate } from "react-router-dom";
+import "styles/card.css"
+const ProfileCard = (props) => {
+  const navigate = useNavigate();   
+  const atHome = props.atHome 
+  const buttonMessage = !atHome ? "back to main page" : `See ${props.data.name}`  
+  const trust_score = !atHome ? <Card.Text>Trust score: {props.data.trust_score}</Card.Text> : null;
+  const year_establish = !atHome ? <Card.Text>Year of establishment: {props.data.year_established}</Card.Text> : null;
+  const link = !atHome ? <Card.Text>Social Media link: {props.data.url}</Card.Text> : null;
+  const description = !atHome ? <Card.Footer> Description: {props.data.description} </Card.Footer> : null;
+  const cardClassName = !atHome ? "card-image-profile" : "card-image-home"
+  return (
+    
+  <Card className="card-container" >
+    <Card.Img variant="top" src={props.data.image} className={cardClassName}/>
+    <Card.Body>
+      <Card.Title>{props.data.name},  found at {props.data.country}</Card.Title> 
+      {trust_score}
+      <Card.Text>Trust rank: {props.data.trust_rank}</Card.Text>
+      {year_establish}
+      {link}
+      {description}
+    </Card.Body>
+    <Button className="route-btn" variant="primary" onClick={()=>navigate(props.nextRoute)}>{buttonMessage}</Button> 
+  </Card>
+  )
+}
+
+export default ProfileCard
