c3d3ec139a7d2ff210ee7e97638a7d2d699a185f
Liaoshousan <shousanl@andrew.cmu.edu>
2022-03-24 22:43:48 -0400

    add comment to config/cors.js and config/urls.js

--- server/config/cors.js
+++ server/config/cors.js
@@ -1,3 +1,8 @@
+/**
+ * To enable request between different ports,   
+ * specify which port is allowed by creating a white list  
+ * and send to cors package  
+ */
 const whitelist = ["http://localhost:3000"]
 exports.corsOptions = {
     origin: function (origin, callback) {
--- server/config/urls.js
+++ server/config/urls.js
@@ -1,3 +1,6 @@
+/**
+ * Export urls as constant
+ */
 module.exports = Object.freeze({
     HOME_API: "https://api.coingecko.com/api/v3/exchanges",
     PROFILE_API: "https://api.coingecko.com/api/v3/exchanges"
